411eadbbbf0d5b9f780d4dee8ffc56eb9e2b5033
ranemihir <mihirrane171@gmail.com>
2021-10-14 11:57:01 +0530

    create deep integration tests

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,47 @@
+import { TestBed } from "@angular/core/testing";
+import { MessageService } from "./message.service";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { HeroService } from "./hero.service";
+
+
+describe('HeroService', () => {
+    let mockMessageService;
+    let httpTestingController: HttpTestingController;
+    let heroService: HeroService;
+
+    beforeEach(() => {
+        mockMessageService = jasmine.createSpyObj(['add']);
+
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                HeroService,
+                {
+                    provide: MessageService,
+                    useValue: mockMessageService
+                }
+            ]
+        });
+
+        httpTestingController = TestBed.inject(HttpTestingController);
+        heroService = TestBed.inject(HeroService);
+    });
+
+    describe('getHero', () => {
+        // call getHero()
+        heroService.getHero(4).subscribe();
+
+        // test that the URL is correct
+        const req = httpTestingController.expectOne('api/heroes/4');
+
+        req.flush({
+            id: 4,
+            name: 'Batman',
+            strength: 40
+        });
+
+        expect(req.request.method).toBe('GET');
+
+        httpTestingController.verify();
+    });
+});
--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -1,23 +1,16 @@
-import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import { of } from "rxjs";
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
+import { HeroComponent } from "../hero/hero.component";
 import { HeroesComponent } from "./heroes.component";
 
-describe('HeroesComponent (shallow tests)', () => {
+describe('HeroesComponent (deep tests)', () => {
     let fixture: ComponentFixture<HeroesComponent>;
     let mockHeroService;
     let HEROES: Hero[];
-
-    @Component({
-        selector: 'app-hero',
-        template: '<div></div>'
-    })
-    class FakeHeroComponent {
-        @Input() hero: Hero;
-    }
 
     beforeEach(() => {
         mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
@@ -28,36 +21,37 @@ describe('HeroesComponent (shallow tests)', () => {
         ];
 
         TestBed.configureTestingModule({
-            declarations: [HeroesComponent, FakeHeroComponent],
+            declarations: [
+                HeroesComponent,
+                HeroComponent
+            ],
             providers: [
                 {
                     provide: HeroService,
                     useValue: mockHeroService
                 }
             ],
+            schemas: [NO_ERRORS_SCHEMA]
         });
 
         fixture = TestBed.createComponent(HeroesComponent);
     });
 
-    it('should set heroes correctly from the service', () => {
+    it('should render each hero as HeroComponent', () => {
         // arrange
         mockHeroService.getHeroes.and.returnValue(of(HEROES));
-        // runs ngOnInit in which getHeroes() method is called
         fixture.detectChanges();
-
-        // assert
-        expect(fixture.componentInstance.heroes).toEqual(HEROES);
-    });
-
-    it('should create one <li> element for each hero', () => {
-        // arrange
 
         // act
-        mockHeroService.getHeroes.and.returnValue(of(HEROES));
-        fixture.detectChanges();
 
         // assert
-        expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+        const heroDebugElement = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+        expect(fixture.componentInstance.heroes.length).toBe(3);
+        expect(heroDebugElement.length).toBe(3);
+
+        for (let i = 0; i < HEROES.length; i++) {
+            expect(heroDebugElement[i].componentInstance.hero).toEqual(HEROES[i]);
+        }
     });
 });
